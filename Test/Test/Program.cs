e5fb47289417431a143ed2574a977b492a5b6f1b
Vartika Khandelwal <vartika.khandelwal@intimetec.com>
2019-05-06 15:34:28 +0530

    added humgry prompt

--- Test/Test/Program.cs
+++ Test/Test/Program.cs
@@ -6,7 +6,17 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Are you hungry");
+            char var = Convert.ToChar(Console.ReadLine());
+            if(var == 'Y')
+            {
+                Console.WriteLine("Eat pizza");
+            }
+            else
+            {
+                Console.WriteLine("Do assignment");
+            }
+            
         }
     }
 }
